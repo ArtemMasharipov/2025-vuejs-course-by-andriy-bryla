a230abc64b7e909c1ba30a4782719146b018f73b
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2025-02-10 13:04:08 +0100

    Update CategoryFilter.vue

--- hw-lesson-7/src/components/CategoryFilter.vue
+++ hw-lesson-7/src/components/CategoryFilter.vue
@@ -1,37 +1,37 @@
 <template>
-  <select 
-    :value="selectedCategory" 
-    :disabled="isLoading"
-    @change="handleChange"
-  >
-    <option v-for="{ id, label } in categories" :key="id" :value="id">
-      {{ label }}
-    </option>
-  </select>
+    <select v-model="currentCategory" :disabled="isLoading">
+        <option v-for="{ id, label } in categories" :key="id" :value="id">
+            {{ label }}
+        </option>
+    </select>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-import { CATEGORIES } from '../constants/grades';
+import { mapGetters, mapActions } from 'vuex'
+import { CATEGORIES } from '../constants/grades'
 
 export default {
-  data: () => ({ selectedCategory: CATEGORIES.ALL.id }),
-  computed: {
-    ...mapGetters(['categories', 'isLoading'])
-  },
-  methods: {
-    ...mapActions(['changeCategory']),
-    handleChange(event) {
-      this.changeCategory(event.target.value);
-    }
-  }
-};
+    computed: {
+        ...mapGetters(['categories', 'isLoading']),
+        currentCategory: {
+            get() {
+                return this.selectedCategory || CATEGORIES.ALL.id
+            },
+            set(value) {
+                this.changeCategory(value)
+            },
+        },
+    },
+    methods: {
+        ...mapActions(['changeCategory']),
+    },
+}
 </script>
 
 <style scoped>
 select:disabled {
-  opacity: 0.6;
-  cursor: not-allowed;
-  background-color: #f5f5f5;
+    opacity: 0.6;
+    cursor: not-allowed;
+    background-color: #f5f5f5;
 }
 </style>
